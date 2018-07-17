d42e54a889b310c70d9fd07b30da640ca2131f48
Alexander Kovalke <mgtherion@gmail.com>
2018-07-17 22:48:29 +0300

    fill board function minified

--- src/app/chess/chess.util.ts
+++ src/app/chess/chess.util.ts
@@ -45,54 +45,18 @@ export function generateEmptyBoard(): Board {
 }
 
 export function fillBoard(board: Board): Board {
-  board[8][0].content = Content.wPawn;
-  board[8][1].content = Content.wPawn;
-  board[8][2].content = Content.wPawn;
-  board[8][3].content = Content.wPawn;
-  board[8][4].content = Content.wPawn;
-  board[8][5].content = Content.wPawn;
-  board[8][6].content = Content.wPawn;
-  board[8][7].content = Content.wPawn;
-  board[8][8].content = Content.wPawn;
-  board[8][9].content = Content.wPawn;
 
-  board[1][0].content = Content.bPawn;
-  board[1][1].content = Content.bPawn;
-  board[1][2].content = Content.bPawn;
-  board[1][3].content = Content.bPawn;
-  board[1][4].content = Content.bPawn;
-  board[1][5].content = Content.bPawn;
-  board[1][6].content = Content.bPawn;
-  board[1][7].content = Content.bPawn;
-  board[1][8].content = Content.bPawn;
-  board[1][9].content = Content.bPawn;
+  for (let i = 0; i < 10; i++) {
+    board[8][i].content = Content.wPawn;
+    board[1][i].content = Content.bPawn;
+    board[9][i].content = Content.wKnight;
+    board[0][i].content = Content.bKnight;
+  }
 
   board[1][0].content = Content.bPawn;
   board[5][5].content = Content.wPawn;
   board[6][5].content = Content.wPawn;
   board[5][4].content = Content.bPawn;
-
-  board[9][0].content = Content.wKnight;
-  board[9][1].content = Content.wKnight;
-  board[9][2].content = Content.wKnight;
-  board[9][3].content = Content.wKnight;
-  board[9][4].content = Content.wKnight;
-  board[9][5].content = Content.wKnight;
-  board[9][6].content = Content.wKnight;
-  board[9][7].content = Content.wKnight;
-  board[9][8].content = Content.wKnight;
-  board[9][9].content = Content.wKnight;
-
-  board[0][0].content = Content.bKnight;
-  board[0][1].content = Content.bKnight;
-  board[0][2].content = Content.bKnight;
-  board[0][3].content = Content.bKnight;
-  board[0][4].content = Content.bKnight;
-  board[0][5].content = Content.bKnight;
-  board[0][6].content = Content.bKnight;
-  board[0][7].content = Content.bKnight;
-  board[0][8].content = Content.bKnight;
-  board[0][9].content = Content.bKnight;
 
   return board;
 }
